9c66f48583dc2e4634b6c988088690fb7dc6f2db
Alejandro Bergesio <55889571+MonsterMaze@users.noreply.github.com>
2026-08-05 16:52:01 -0300

    Add files via upload

---
+++ software/build/CMakeFiles/EpQt.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EpQt_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/EpQt_autogen.dir/ParseCache.txt"
+  "EpQt_autogen"
+  "CMakeFiles/EpQt.dir/EpQt_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/EpQt.dir/EpQt_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/EpQt.dir/main.cpp.o"
+  "CMakeFiles/EpQt.dir/main.cpp.o.d"
+  "CMakeFiles/EpQt.dir/src/ArduinoIO.cpp.o"
+  "CMakeFiles/EpQt.dir/src/ArduinoIO.cpp.o.d"
+  "CMakeFiles/EpQt.dir/src/QtGui.cpp.o"
+  "CMakeFiles/EpQt.dir/src/QtGui.cpp.o.d"
+  "CMakeFiles/EpQt.dir/src/eprom.cpp.o"
+  "CMakeFiles/EpQt.dir/src/eprom.cpp.o.d"
+  "EpQt"
+  "EpQt.pdb"
+  "EpQt_autogen/mocs_compilation.cpp"
+  "EpQt_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EpQt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
