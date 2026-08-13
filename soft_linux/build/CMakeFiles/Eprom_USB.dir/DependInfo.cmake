2dd38a67d2f5ed1cec9855432a7f8b2579ab0d3c
Alejandro Rafael Miguel Bergesio <55889571+MonsterMaze@users.noreply.github.com>
2026-08-13 11:47:26 -0300

    Add files via upload

---
+++ soft_linux/build/CMakeFiles/Eprom_USB.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "Eprom_USB_autogen/timestamp" "custom" "Eprom_USB_autogen/deps"
+  "/home/alex/Documentos/EpromProgrammer-main/gui/build/Eprom_USB_autogen/mocs_compilation.cpp" "CMakeFiles/Eprom_USB.dir/Eprom_USB_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Eprom_USB.dir/Eprom_USB_autogen/mocs_compilation.cpp.o.d"
+  "/home/alex/Documentos/EpromProgrammer-main/gui/main.cpp" "CMakeFiles/Eprom_USB.dir/main.cpp.o" "gcc" "CMakeFiles/Eprom_USB.dir/main.cpp.o.d"
+  "/home/alex/Documentos/EpromProgrammer-main/gui/src/ArduinoIO.cpp" "CMakeFiles/Eprom_USB.dir/src/ArduinoIO.cpp.o" "gcc" "CMakeFiles/Eprom_USB.dir/src/ArduinoIO.cpp.o.d"
+  "/home/alex/Documentos/EpromProgrammer-main/gui/src/QtGui.cpp" "CMakeFiles/Eprom_USB.dir/src/QtGui.cpp.o" "gcc" "CMakeFiles/Eprom_USB.dir/src/QtGui.cpp.o.d"
+  "/home/alex/Documentos/EpromProgrammer-main/gui/src/eprom.cpp" "CMakeFiles/Eprom_USB.dir/src/eprom.cpp.o" "gcc" "CMakeFiles/Eprom_USB.dir/src/eprom.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ soft_linux/build/CMakeFiles/Eprom_USB_autogen_timestamp_deps.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for Eprom_USB_autogen_timestamp_deps.
